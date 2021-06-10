1c2726b518d0af1fd60c6fc5d1f800b6609b8fbe
Samar Qureshi <63826930+samarqureshii@users.noreply.github.com>
2021-06-10 14:00:09 -0400

    Update 4_LEDs.ino

--- 4_LEDs.ino
+++ 4_LEDs.ino
@@ -1,1 +1,14 @@
-int led1 = 1;void setup() {  pinMode(led1, OUTPUT);}void loop() {    digitalWrite(led1, HIGH);  delay(500);}
+int led1 = 1;
+
+void setup() {
+  pinMode(led1, OUTPUT);
+
+}
+
+void loop() {  
+  digitalWrite(led1, HIGH);
+  delay(500);
+
+
+}
+
